2c9a353b6c5308d55b5bbe1d72e47b9c905bd95f
Kenneth Ofosu <kofosu89@gmail.com>
2020-09-01 16:51:06 -0400

    create private feeding_parmas method in controller

--- app/controllers/feedings_controller.rb
+++ app/controllers/feedings_controller.rb
@@ -3,4 +3,10 @@ class FeedingsController < ApplicationController
     @feedings = Feeding.all
     render json: @feedings, status: :ok
   end
+
+  private
+
+  def feeding_params
+    params.require(:feeding).permit(:food, :datetime)
+  end
 end
