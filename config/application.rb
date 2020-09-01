0137703718ff0b009f8f56af82f45c2356f529e0
Kenneth Ofosu <kofosu89@gmail.com>
2020-09-01 19:09:13 -0400

    heroku issues

--- config/application.rb
+++ config/application.rb
@@ -33,11 +33,11 @@ module FeedingtimeApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.middleware.use Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :put, :options, :delete], :credentials => false
-      end
-    end
+    # config.middleware.use Rack::Cors do
+    #   allow do
+    #     origins '*'
+    #     resource '*', :headers => :any, :methods => [:get, :post, :put, :options, :delete], :credentials => false
+    #   end
+    # end
   end
 end
