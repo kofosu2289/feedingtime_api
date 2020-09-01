39571e3fd2d5039507aa7dbaf33ed00e17efea94
Kenneth Ofosu <kofosu89@gmail.com>
2020-09-01 17:00:35 -0400

    format timestamps to fill datetime column; ready for ui

--- app/models/feeding.rb
+++ app/models/feeding.rb
@@ -1,2 +1,7 @@
 class Feeding < ApplicationRecord
+  def as_json(options={})
+    {id: id, food: food, datetime: datetime, created_at: created_at}.tap do |hash|
+      hash[:datetime] = created_at.in_time_zone("Eastern Time (US & Canada)").strftime("%m/%d/%Y at %I:%M %p")
+    end
+  end
 end
